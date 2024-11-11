cf41d7c7ae32afc94fe15b964c28573249f5a940
Rahul Tiwari <rahul.tiwari@fdmgroup.com>
2024-11-11 17:23:53 -0500

    Enable CORS

--- src/main/java/com/rahul/movies/controller/MovieController.java
+++ src/main/java/com/rahul/movies/controller/MovieController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.rahul.movies.service.MovieService;
 
 @RestController
 @RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "*")
 public class MovieController {
 	
 	private MovieService movieService;
